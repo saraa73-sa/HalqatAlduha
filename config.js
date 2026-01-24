f7b371a3af95b09eaea8b254a6f25f93f78b6612
saraa73-sa <saraa777733@gmail.com>
2026-01-24 11:01:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby-KVV8gkgdcE4oaNi29cYHYxEXezIvgf2xcARRXK9IWzxMEDhTIko76XYkhFxJYGoh/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxl2EPw5t1rW8Sh2aITa8PPZVWxEgc0czQIvqLYGQaUEcq4hnYQ3fSqv0N6PcGLlSJZ/exec";
